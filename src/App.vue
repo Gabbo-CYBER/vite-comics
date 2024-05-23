de3dc3b13d7d8464c33e45a9f618d7fe0f59c356
Your Name <you@example.com>
2024-05-23 20:36:38 +0200

    m-1 first commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+
+export default {
+  components: {
+    AppHeader,
+    AppMain,
+    AppFooter
+  },
+  data() {
+    return {
+      
+    }
+  },
+  methods: {
+   
+    }
+  }
+ 
+</script>
+
+<template>
+  <AppHeader />
+  <!-- <a href="google.it">Link non stilizzato in header</a> -->
+
+  <main>
+    
+  </main>
+
+  <AppFooter />
+      
+</template>
+
+<style scoped>
+
+</style>
